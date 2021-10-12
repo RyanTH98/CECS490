d38a6304b0c576cfbb53a6010e0d64dc514d9a8f
Graham <bggillmore@gmail.com>
2021-10-12 16:08:22 -0700

    Created header file and debugged to the point where it compiles.

--- Test Projects/virtual_board/main.c
+++ Test Projects/virtual_board/main.c
@@ -1,27 +1,32 @@
 // Author:  Alex Gonzalez
 // Project: Smart Chess Board
 
+#include "main.h"
+#include <stdio.h>
+
+//EDIT: Put in header file (bg)
+
 // Piece
-typedef enum type { NONE, PAWN, BISHOP, KNIGHT, ROOK, QUEEN, KING } type;
-
-typedef enum color { NONE, BLACK, WHITE } color;
-
-typedef struct _piece {
-	type  type		= NONE;
-	color color		= NONE;
-	int   row	    = -1;
-	int   column    = -1;
-	char  direction = 'X';
-	bool  firstMove = true;
-} piece;
+//typedef enum type { NONE, PAWN, BISHOP, KNIGHT, ROOK, QUEEN, KING } type;
+//
+//typedef enum color { NONE, BLACK, WHITE } color;
+//
+//typedef struct _piece {
+//	type  type		= NONE;
+//	color color		= NONE;
+//	int   row	    = -1;
+//	int   column    = -1;
+//	char  direction = 'X';
+//	bool  firstMove = true;
+//} piece;
 
 
 
 int main(void) {
 
-	piece board[7][7];
+	_piece board[7][7];
 
-	reset_board(&board);
+	//reset_board(board);
 
 	printf("Test");
 
@@ -29,51 +34,55 @@ int main(void) {
 }
 
 
-void get_moves(piece *board, piece *p) {
-	switch(p) {
-		case NONE:
+void get_moves(_piece (*board)[7], _piece *p) {
+	switch(p->type) {
+		case NO_PIECE:
 			break;
 		case PAWN:
 			/* If a piece is moving DOWN the board */
 			if (p->direction == 'D') {
 
 				/* Check BELOW for a piece */
-				if (check_legal(board[p->row - 1][p->column]))
+				if (check_legal(&board[p->row - 1][p->column]))
 					// Light up square in white
 
 				/* Check BELOW, DIAGONALLY for a piece */
-				if (!check_legal(board[p->row - 1][p->column - 1]))
+				if (!check_legal(&board[p->row - 1][p->column - 1]))
 					// Light up square in red
-				if (!check_legal(board[p->row - 1][p->column + 1]))
+				if (!check_legal(&board[p->row - 1][p->column + 1]))
 					// Light up square in red
 
 
 				/* Special Case */
 				/* FIRST move ONLY - Check two squares BELOW for a piece */
-				if (p->firstMove == true)
-					if (check_legal(board[p->row - 2][p->column]))
+				if (p->firstMove == true){
+					if (check_legal(&board[p->row - 2][p->column])){
 						// Light up square in white/
-		    }
-
+					}
+				}
+		    	}
+			
 			/* If a piece is moving UP the board */
 			else if (p->direction == 'D') {
 
 				/* Check ABOVE for a piece */
-				if (check_legal(board[p->row + 1][p->column]))
+				if (check_legal(&board[p->row + 1][p->column]))
 					// Light up square in white
 
 				/* Check ABOVE, DIAGONALLY for a piece */
-				if (!check_legal(board[p->row + 1][p->column - 1]))
+				if (!check_legal(&board[p->row + 1][p->column - 1]))
 					// Light up square in red
-				if (!check_legal(board[p->row + 1][p->column + 1]))
+				if (!check_legal(&board[p->row + 1][p->column + 1]))
 					// Light up square in red
 
 							
 				/* Special Case */
 				/* FIRST move ONLY - Check two squares BELOW for a piece */
-				if (p->firstMove == true)
-					if (check_legal(board[p->row - 2][p->column]))
+				if (p->firstMove == true){
+					if (check_legal(&board[p->row - 2][p->column])){
 						// Light up square in white/
+					}
+				}
 			}
 			break;
 		case BISHOP:
@@ -82,7 +91,7 @@ void get_moves(piece *board, piece *p) {
 			/* BELOW and to the RIGHT */
 			for (int i = p->row; i < 8; i++) {
 				for (int j = p->column; j < 8; j++) {
-					if (check_legal(board[i][j])) {
+					if (check_legal(&board[i][j])) {
 						// Light up square in white
 					} else {
 						// Light up square in red
@@ -94,7 +103,7 @@ void get_moves(piece *board, piece *p) {
 			/* BELOW and to the LEFT */
 			for (int i = p->row; i < 8; i++) {
 				for (int j = p->column; j >= 0; j--) {
-					if (check_legal(board[i][j])) {
+					if (check_legal(&board[i][j])) {
 						// Light up square in white
 					} else {
 						// Light up square in red
@@ -106,7 +115,7 @@ void get_moves(piece *board, piece *p) {
 			/* ABOVE and to the Right */
 			for (int i = p->row; i >= 0; i--) {
 				for (int j = p->column; j > 8; j++) {
-					if (check_legal(board[i][j])) {
+					if (check_legal(&board[i][j])) {
 						// Light up square in white
 					} else {
 						// Light up square in red
@@ -118,7 +127,7 @@ void get_moves(piece *board, piece *p) {
 			/* ABOVE and to the LEFT */
 			for (int i = p->row; i >= 0; i--) {
 				for (int j = p->column; j >= 0; j--) {
-					if (check_legal(board[i][j])) {
+					if (check_legal(&board[i][j])) {
 						// Light up square in white
 					} else {
 						// Light up square in red
@@ -134,7 +143,7 @@ void get_moves(piece *board, piece *p) {
 
 			/* LEFT, until a piece is detected */
 			for (int j = p->column; j <= 0; j--) {
-				if (check_legal(board[p->row][j])) {
+				if (check_legal(&board[p->row][j])) {
 					// Light up square in white
 				} else {
 					// Light up square in red
@@ -144,7 +153,7 @@ void get_moves(piece *board, piece *p) {
 
 			/* RIGHT, until a piece is detected */
 			for (int j = p->column; j > 8; j++) {
-				if (check_legal(board[p->row][j])) {
+				if (check_legal(&board[p->row][j])) {
 					// Light up square in white
 				} else {
 					// Light up square in red
@@ -154,9 +163,9 @@ void get_moves(piece *board, piece *p) {
 
 			/* Special Case */
 			/* FIRST move ONLY - Check for King to castle */
-			piece p1 = board[p - row][p->column - 3];
-			piece p2 = board[p - row][p->column - 2];
-			piece p3 = board[p - row][p->column - 1];
+			_piece* p1 = &board[p -> row][p->column - 3];
+			_piece* p2 = &board[p -> row][p->column - 2];
+			_piece* p3 = &board[p -> row][p->column - 1];
 			
 			/* Check if King is in the proper place, and squares in between Rook and King are empty */
 			if (p1->type == KING && check_legal(p2) && check_legal(p3)) {
@@ -169,7 +178,7 @@ void get_moves(piece *board, piece *p) {
 			/* BELOW and to the RIGHT */
 			for (int i = p->row; i < 8; i++) {
 				for (int j = p->column; j < 8; j++) {
-					if (check_legal(board[i][j])) {
+					if (check_legal(&board[i][j])) {
 						// Light up square in white
 					}
 					else {
@@ -182,7 +191,7 @@ void get_moves(piece *board, piece *p) {
 			/* BELOW and to the LEFT */
 			for (int i = p->row; i < 8; i++) {
 				for (int j = p->column; j >= 0; j--) {
-					if (check_legal(board[i][j])) {
+					if (check_legal(&board[i][j])) {
 						// Light up square in white
 					}
 					else {
@@ -195,7 +204,7 @@ void get_moves(piece *board, piece *p) {
 			/* ABOVE and to the Right */
 			for (int i = p->row; i >= 0; i--) {
 				for (int j = p->column; j > 8; j++) {
-					if (check_legal(board[i][j])) {
+					if (check_legal(&board[i][j])) {
 						// Light up square in white
 					}
 					else {
@@ -208,7 +217,7 @@ void get_moves(piece *board, piece *p) {
 			/* ABOVE and to the LEFT */
 			for (int i = p->row; i >= 0; i--) {
 				for (int j = p->column; j >= 0; j--) {
-					if (check_legal(board[i][j])) {
+					if (check_legal(&board[i][j])) {
 						// Light up square in white
 					}
 					else {
@@ -223,7 +232,7 @@ void get_moves(piece *board, piece *p) {
 
 			/* LEFT, until a piece is detected */
 			for (int j = p->column; j <= 0; j--) {
-				if (check_legal(board[p->row][j])) {
+				if (check_legal(&board[p->row][j])) {
 					// Light up square in white
 				}
 				else {
@@ -234,7 +243,7 @@ void get_moves(piece *board, piece *p) {
 
 			/* RIGHT, until a piece is detected */
 			for (int j = p->column; j > 8; j++) {
-				if (check_legal(board[p->row][j])) {
+				if (check_legal(&board[p->row][j])) {
 					// Light up square in white
 				}
 				else {
@@ -258,11 +267,11 @@ void illuminate(piece* p, COLOR) {
 }
 */
 
-bool check_legal(piece *p) {
-	return (p.type == NULL);
+bool check_legal(_piece *p) {
+	return (p->type == NO_PIECE);
 }
 
-void reset_board(piece *board) {
+void reset_board(_piece(*board)[7]) {
 
 	/* Assign color of all Black pieces
 	   Set default direction to moving downward */
@@ -317,12 +326,12 @@ void reset_board(piece *board) {
 
 }
 
-void move_piece(piece *board, piece *p, int x, int y) {
-	if (p.firstMove == true)
-		p.firstMove == false
+void move_piece(_piece (*board)[7], _piece *p, int x, int y) {
+	if (p->firstMove == true)
+		p->firstMove == false;
 
-	board[x][y] = p;
+	board[x][y] = *p;
 
-	p.type = NONE;
-	p.color = NONE;
+	p->type = NO_PIECE;
+	p->color = NO_COLOR;
 }
---
+++ Test Projects/virtual_board/main.h
@@ -0,0 +1,47 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  10/12/2021 02:43:39 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *        Authors:  Alex Gonzalez(ag), Benjamin Gillmore (bg), bggillmore@gmail.com
+ *        Company:  California State University Long Beach
+ *
+ * =====================================================================================
+ */
+#include <stdbool.h>
+
+typedef enum _type { NO_PIECE, PAWN, BISHOP, KNIGHT, ROOK, QUEEN, KING } _type;
+
+typedef enum _color { NO_COLOR, BLACK, WHITE } _color;
+
+typedef struct _piece {
+	_type type;
+	_color color;
+	int row;
+	int column;
+	char direction;
+	bool firstMove;
+} _piece;
+
+
+//Description
+void get_moves(_piece(*)[7],  _piece*);
+
+//Description
+//void illuminate(piece, COLOR);
+
+//Description
+bool check_legal(_piece*);
+
+//Description
+void reset_board(_piece(*)[7]);
+
+//Description
+void move_piece(_piece(*)[7], _piece*, int, int);
