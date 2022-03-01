c03d472a7585c3d1419dfc6e7101cfdeaff8973b
Graham <bggillmore@gmail.com>
2022-03-01 15:28:23 -0800

    added led test, pushing before recursevely pulling submodules to be safe

--- tests/main/mux_test.cpp
+++ tests/main/mux_test.cpp
@@ -4,15 +4,20 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
-//#include "Arduino.h"
-//#include <Adafruit_NeoPixel.h>
-//#define ESP32
+#include "Arduino.h"
+#include <Adafruit_NeoPixel.h>
+
+//compiler definitions
+#define NUM_PIXELS 4
 
 
 //function prototypes
 void mux_test();
 void decoder_test();
+void led_test();
+void init()
 void delay_1ms(int);
+
 
 //globals
 //mux
@@ -30,33 +35,19 @@ gpio_num_t decoderA2 = GPIO_NUM_25;
 gpio_num_t decoderEn = GPIO_NUM_33;
 unsigned short decoder_counter = 0;
 
+//led
+gpio_num_t led_strip = GPIO_NUM_23;
+Adafruit_NeoPixel pixels(NUMPIXELS, led_strip, NEO_GRB + NEO_KHZ800);
+
+//other
 gpio_mode_t out = GPIO_MODE_OUTPUT;
 gpio_mode_t in = GPIO_MODE_INPUT;
 
 
+//main
 extern "C" void app_main(){
 	printf("CODE BEGIN\n");
-	
-        gpio_reset_pin(muxEn_n);
-        gpio_reset_pin(muxA);
-        gpio_reset_pin(muxB);
-        gpio_reset_pin(muxC);
-        gpio_reset_pin(muxY);
-        gpio_reset_pin(decoderA0);
-        gpio_reset_pin(decoderA1);
-        gpio_reset_pin(decoderA2);
-        gpio_reset_pin(decoderEn);
-        gpio_set_direction(muxEn_n, out);
-        gpio_set_direction(muxA, out);
-        gpio_set_direction(muxB, out);
-        gpio_set_direction(muxC, out);
-        gpio_set_direction(muxY, in);
-        gpio_set_direction(decoderA0, out);
-        gpio_set_direction(decoderA1, out);
-        gpio_set_direction(decoderA2, out);
-        gpio_set_direction(decoderEn, out);
-	
-
+	init();
 	printf("turning on mux\n");
 	gpio_set_level(muxEn_n, 0);
     	
@@ -68,7 +59,8 @@ extern "C" void app_main(){
 		//printf("Runnin\n");
 		delay_1ms(1000);
 		//mux_test();
-		decoder_test(); 
+		//decoder_test();
+		led_test();
     	}
 }
 
@@ -97,6 +89,43 @@ void decoder_test(){
 	decoder_counter = decoder_counter%8;
 }
 
+void led_test(){
+	pixels.clear(); // Set all pixel colors to 'off'
+	
+	//set every other pixel on
+  	for(int i=0; i<NUMPIXELS; i++) {
+		printf("Setting Led #%d to %d\n", i, i%2);
+		if(i%2 == 0){ //off
+    			pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+		}
+		else{ //on    		
+			pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+    		}
+	}
+	pixels.show();   // Send the updated pixel colors to the hardware.
+}
+
 void delay_1ms(int duration) { vTaskDelay(duration / portTICK_PERIOD_MS); }
 
+void init(){
+        gpio_reset_pin(muxEn_n);
+        gpio_reset_pin(muxA);
+        gpio_reset_pin(muxB);
+        gpio_reset_pin(muxC);
+        gpio_reset_pin(muxY);
+        gpio_reset_pin(decoderA0);
+        gpio_reset_pin(decoderA1);
+        gpio_reset_pin(decoderA2);
+        gpio_reset_pin(decoderEn);
+        gpio_set_direction(muxEn_n, out);
+        gpio_set_direction(muxA, out);
+        gpio_set_direction(muxB, out);
+        gpio_set_direction(muxC, out);
+        gpio_set_direction(muxY, in);
+        gpio_set_direction(decoderA0, out);
+        gpio_set_direction(decoderA1, out);
+        gpio_set_direction(decoderA2, out);
+        gpio_set_direction(decoderEn, out);
+	pixels.begin();
+}
 
